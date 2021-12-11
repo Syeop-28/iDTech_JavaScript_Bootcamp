0149e76c7bea8ec1a13231375165996bb72ef80f
Santiago Yeomans <syeomansp@gmail.com>
2021-12-11 11:13:54 -0600

    commir

---
+++ Basic Data Types Activity/story_time_SantiagoY.js
@@ -0,0 +1,29 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let genre="SciFi";
+let setting="Mercury";
+let aliens="Space vampire"
+let main_character="James Canpel";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let year= 3055;
+let age_of_protagonist= 35;
+let time_to_save_the_galaxy= 10;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+let Alien_hunter_tools=["laser zapper","Light blue paracord","Can of orange juice","Water generator","Nutrient packets","Diary","Universal translator"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+
+let mission_succesful=true
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(`This ${genre} story takes place in ${setting} in the year ${year}`)
+console.log(`It tells the story of ${age_of_protagonist} year old ${aliens} hunter ${main_character} and his quest to save the galaxy in ${time_to_save_the_galaxy} days`)
+console.log("He might make it or not it depends on what he chooses to take with him");
+console.log(`He chose ${Alien_hunter_tools[0]}`);
+console.log(`Mission succesfull...${mission_succesful} `);
